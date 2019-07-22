26253d08aa06dc85b099c9add0d16e2803d2ddd2
Vibe <s.dmitrievlol@yandex.ru>
2019-07-22 23:47:58 +0300

    second commit

--- whbot.py
+++ whbot.py
@@ -15,9 +15,11 @@ def write_js(data, fname='response.json'):
         json.dump(data, f, indent=2)
 
 '''
+'''
 prox = {'https' :  '195.122.185.95:3128',
         'SOCKS5' : '139.59.169.246:1080'
  }
+ '''
 greetings = ['привет', 'хай', 'хелло', 'hello', 'hi', 'дратути', 'здравствуй','hey']
 
 class Bot():
@@ -29,7 +31,7 @@ class Bot():
     def send_message(self, chat_id, text):
         params = {'chat_id' : chat_id, 'text' : text}
         meth = 'sendMessage'
-        answ = requests.get(self.url + meth, params=params, proxies=prox)
+        answ = requests.get(self.url + meth, params=params)
         return answ
     def write_log(self, data, fname='logfile.txt'):
         with open(fname, 'a') as f:
@@ -53,20 +55,17 @@ def resp():
         user = r['message']['from']['first_name']
         if message.lower() == 'кисик':
             my_bot.send_message(chat_id, get_random_cat())
-        if message.lower() in greetings:
+        elif message.lower() in greetings:
             my_bot.send_message(chat_id, 'Дратути!')
+        else:
+            my_bot.send_message(chat_id, 'Я не понял(((')
         log_message = 'В {} {} написал(а) {}'.format(time.strftime("%Y-%m-%d %H:%M:%S"), user, message)
         my_bot.write_log(log_message)
+        
 
         return '<h1>wewew</h1>'
     else:
         return '<h1>Вжух</h1>'
-
-
-
-
-
-    
 
 if __name__ == "__main__":
     app.run()
